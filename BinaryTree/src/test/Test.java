9cd140a12b900430b6b7599b6455f4b7ff91872a
ErickCR12 <erick.cr12@outlook.com>
2018-10-10 00:45:20 -0600

    main test for avl tree

--- BinaryTree/src/test/Test.java
+++ BinaryTree/src/test/Test.java
@@ -5,24 +5,24 @@ import dataStructures.BinarySearchTree;
 
 public class Test {
     public static void main(String[] args){
-        AVLTree bts = new AVLTree();
-        bts.insertNode(100);
-        bts.insertNode(200);
-        bts.insertNode(150);
-        bts.insertNode(300);
-        bts.insertNode(50);
-        bts.insertNode(75);
-        bts.insertNode(25);
-        bts.insertNode(10);
-        bts.insertNode(5);
-        bts.insertNode(7);
-        bts.insertNode(8);
-        bts.insertNode(350);
-        bts.insertNode(400);
-        bts.infixPrint(bts.getRoot());
-        bts.deleteNode(25);
+        AVLTree avl = new AVLTree();
+        avl.insertNode(100);
+        avl.insertNode(200);
+        avl.insertNode(150);
+        avl.insertNode(300);
+        avl.insertNode(50);
+        avl.insertNode(75);
+        avl.insertNode(25);
+        avl.insertNode(10);
+        avl.insertNode(5);
+        avl.insertNode(7);
+        avl.insertNode(8);
+        avl.insertNode(350);
+        avl.insertNode(400);
+        avl.infixPrint(avl.getRoot());
+        avl.deleteNode(25);
         System.out.println("\n\n\n\n");
-        bts.infixPrint(bts.getRoot());
+        avl.infixPrint(avl.getRoot());
 
     }
 }
